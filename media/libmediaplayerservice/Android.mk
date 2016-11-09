ea54a7ee5356350b57f667b7553aa72688e6aac0
Vinzenz Hersche <hersche@openmailbox.org>
2016-11-09 18:56:44 +0100

    add commit "Add mtk FMRadio support" from fire855 by hand repo: https://github.com/fire855/android_frameworks_av-mtk (not directly mergable)

--- media/libmediaplayerservice/Android.mk
+++ media/libmediaplayerservice/Android.mk
@@ -57,7 +57,13 @@ LOCAL_C_INCLUDES :=                                                 \
     $(TOP)/frameworks/native/include/media/openmax                  \
     $(TOP)/external/tremolo/Tremolo                                 \
     $(TOP)/frameworks/av/media/libavextensions                      \
-
+    
+    
+# Mediatek
+ifeq ($(strip $(BOARD_HAS_MTK_HARDWARE)),true)
+LOCAL_SHARED_LIBRARIES += libmtkplayer
+endif
+    
 LOCAL_CFLAGS += -Werror -Wno-error=deprecated-declarations -Wall #-DLOG_NDEBUG=0
 LOCAL_CLANG := true
 
